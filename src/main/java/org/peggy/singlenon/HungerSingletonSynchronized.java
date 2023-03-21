dd5bd1c6cff868146ebfd1c944dbee69bb61b390
Peggy <94164254+Peggy-M@users.noreply.github.com>
2023-03-21 19:09:34 +0800

    Update HungerSingletonSynchronized.java
    饿汉式单利类对象修改

--- src/main/java/org/peggy/singlenon/HungerSingletonSynchronized.java
+++ src/main/java/org/peggy/singlenon/HungerSingletonSynchronized.java
@@ -16,7 +16,7 @@ public class HungerSingletonSynchronized {
     //如果不存在才进行对象的创建
     public static HungerSingletonSynchronized getHungerSingleton() {
         if (hungerSingletonSynchronized == null) {
-            synchronized (hungerSingletonSynchronized){
+            synchronized (HungerSingletonSynchronized.class){
                 //这里存在线程的安全问题,假设线程 A 执行到此位置，这个时候线程 B 在 synchronized 的位置等待
                 //这个时候线程 A 已经处理完毕创建了一个当前的对象，并且释放了锁
                 //线程 B 获取到了锁,但是由于现象 B 已经经历过了当前的对象的非空判断,这个时候线程 B 继续执行就会创建一个新的对象
